bec43ce3a279f5eb4e26aaf92db99696e332b0cd
mateja <mateja176@gmail.com>
2020-07-06 15:00:53 +0200

    refactor(containers/Idea): moves Tabs to IdeasPage

--- src/containers/Idea/Ideas/Ideas.tsx
+++ src/containers/Idea/Ideas/Ideas.tsx
@@ -1,231 +1,184 @@
-import { Box, Tab, Tabs, Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { CreateIdea, IdeaRow, IdeaSkeleton } from 'containers';
-import { IdeaFilter } from 'models';
-import qs from 'qs';
+import { IdeaFilter, User } from 'models';
 import { range } from 'ramda';
 import React from 'react';
 import { connect, useDispatch } from 'react-redux';
-import { useHistory, useLocation } from 'react-router-dom';
 import { AutoSizer, InfiniteLoader, List } from 'react-virtualized';
 import {
   createPromisedAction,
-  createSetCount,
-  createSetIdeas,
   fetchIdeasAsync,
   IdeasState,
-  initialIdeasState,
   selectCount,
   selectIdeas,
   State,
   useActions,
-  useSignedInUser,
 } from 'services';
 import { ideaListItemFullHeight } from 'styles';
 import { getType } from 'typesafe-actions';
 import { IdeaOptionsSkeleton } from '../IdeaOptionsSkeleton';
 
-export interface IdeasProps extends Pick<IdeasState, 'ideas' | 'count'> {}
+export interface IdeasProps extends Pick<IdeasState, 'ideas' | 'count'> {
+  showMyIdeas: boolean;
+  user: User;
+}
 
-export const IdeasComponent: React.FC<IdeasProps> = ({ ideas, count }) => {
-  const dispatch = useDispatch();
+export const IdeasComponent = React.forwardRef<InfiniteLoader, IdeasProps>(
+  ({ showMyIdeas, user, ideas, count }, infiniteLoaderRef) => {
+    const rowCount = count;
 
-  const { fetchIdeas, setIdeas, setCount } = useActions({
-    fetchIdeas: fetchIdeasAsync.request,
-    setIdeas: createSetIdeas,
-    setCount: createSetCount,
-  });
+    const dispatch = useDispatch();
 
-  const rowCount = count;
+    const { fetchIdeas } = useActions({
+      fetchIdeas: fetchIdeasAsync.request,
+    });
 
-  const listWrapperRef = React.useRef<HTMLDivElement | null>(null);
+    const listWrapperRef = React.useRef<HTMLDivElement | null>(null);
 
-  const [listWrapperHeight, setListWrapperHeight] = React.useState<
-    React.CSSProperties['height']
-  >('auto');
+    const [listWrapperHeight, setListWrapperHeight] = React.useState<
+      React.CSSProperties['height']
+    >('auto');
 
-  React.useEffect(() => {
-    if (listWrapperRef.current) {
-      const { top } = listWrapperRef.current.getBoundingClientRect();
+    React.useEffect(() => {
+      if (listWrapperRef.current) {
+        const { top } = listWrapperRef.current.getBoundingClientRect();
 
-      setListWrapperHeight(window.innerHeight - top);
-    }
-  }, []);
+        setListWrapperHeight(window.innerHeight - top);
+      }
+    }, []);
 
-  const history = useHistory();
+    const myIdeasFilter: IdeaFilter<'author'> = {
+      fieldPath: 'author',
+      opStr: '==',
+      value: user?.email || '',
+    };
 
-  const user = useSignedInUser();
+    const publishedFilter: IdeaFilter<'status'> = {
+      fieldPath: 'status',
+      opStr: '==',
+      value: 'sprout',
+    };
 
-  const query = qs.parse(useLocation().search, { ignoreQueryPrefix: true });
+    const filter = showMyIdeas ? myIdeasFilter : publishedFilter;
 
-  const showMyIdeas = query.author === user?.email;
-
-  const myIdeasFilter: IdeaFilter<'author'> = {
-    fieldPath: 'author',
-    opStr: '==',
-    value: user?.email || '',
-  };
-
-  const publishedFilter: IdeaFilter<'status'> = {
-    fieldPath: 'status',
-    opStr: '==',
-    value: 'sprout',
-  };
-
-  const filter = showMyIdeas ? myIdeasFilter : publishedFilter;
-
-  const infiniteLoaderRef = React.useRef<InfiniteLoader | null>(null);
-
-  const reset = () => {
-    setIdeas({ ideas: [] });
-    setCount({ count: initialIdeasState.count });
-
-    infiniteLoaderRef.current?.resetLoadMoreRowsCache();
-  };
-
-  const [activeTab, setActiveTab] = React.useState(Number(showMyIdeas));
-
-  return (
-    <Box>
-      <Tabs
-        value={activeTab}
-        onChange={(e, value) => {
-          setActiveTab(value);
-        }}
-        indicatorColor="primary"
-        textColor="primary"
-        variant="fullWidth"
-      >
-        <Tab
-          label="Discover"
-          onClick={() => {
-            reset();
-
-            history.push({
-              search: qs.stringify({}),
-            });
-          }}
-        />
-        <Tab
-          label="Your Ideas"
-          onClick={() => {
-            reset();
-
-            history.push({
-              search: qs.stringify({ author: user?.email }),
-            });
-          }}
-        />
-      </Tabs>
-      <div
-        ref={listWrapperRef}
-        style={{ height: listWrapperHeight, overflow: 'auto' }}
-      >
-        <InfiniteLoader
-          ref={infiniteLoaderRef}
-          loadMoreRows={(indexRange) => {
-            const fetchOptions = { ...filter, ...indexRange };
-
-            fetchIdeas(fetchOptions);
-
-            return new Promise((resolve) => {
-              dispatch(
-                createPromisedAction({
-                  type: getType(fetchIdeasAsync.success),
-                  callback: resolve,
-                }),
-              );
-            });
-          }}
-          isRowLoaded={({ index }) => {
-            return !!ideas[index];
-          }}
-          rowCount={rowCount}
+    return (
+      <Box>
+        <div
+          ref={listWrapperRef}
+          style={{ height: listWrapperHeight, overflow: 'auto' }}
         >
-          {({ onRowsRendered, registerChild }) => (
-            <AutoSizer>
-              {({ width, height }) => (
-                <List
-                  rowCount={rowCount}
-                  ref={registerChild}
-                  onRowsRendered={onRowsRendered}
-                  width={width}
-                  height={height}
-                  rowHeight={ideaListItemFullHeight}
-                  noRowsRenderer={() =>
-                    showMyIdeas ? (
-                      <Box m={3}>
-                        <Typography variant="h6">
-                          I have no idea{' '}
-                          <span role="img" aria-label="thinking">
-                            🤔
-                          </span>{' '}
-                          Do you?
-                        </Typography>
-                        <Box mt={2}>
-                          <CreateIdea user={user} />
-                        </Box>
-                      </Box>
-                    ) : (
-                      <Box>
-                        {range(0, count).map(() => (
-                          <IdeaSkeleton />
-                        ))}
-                      </Box>
-                    )
-                  }
-                  rowRenderer={({ key, index, style }) => {
-                    const idea = ideas[index];
+          <InfiniteLoader
+            ref={infiniteLoaderRef}
+            loadMoreRows={(indexRange) => {
+              const fetchOptions = { ...filter, ...indexRange };
 
-                    return (
-                      <Box key={key} style={style}>
-                        {!idea ? null : idea === 'loading' ? (
-                          <IdeaOptionsSkeleton />
-                        ) : idea instanceof Error ? (
-                          <Box
-                            display="flex"
-                            alignItems="center"
-                            height="100%"
-                            mx={2}
-                          >
-                            <Typography
-                              style={{
-                                textDecoration: 'underline',
-                                cursor: 'pointer',
-                              }}
-                              onClick={() => {
-                                const fetchOptions = {
-                                  startIndex: idea.startIndex,
-                                  stopIndex: idea.stopIndex,
-                                  fieldPath: idea.fieldPath,
-                                  opStr: idea.opStr,
-                                  value: idea.value,
-                                };
-                                fetchIdeas(fetchOptions);
-                              }}
-                            >
-                              Refetch ideas
-                            </Typography>
-                            &nbsp; from {idea.startIndex + 1} to{' '}
-                            {idea.stopIndex + 1}
+              fetchIdeas(fetchOptions);
+
+              return new Promise((resolve) => {
+                dispatch(
+                  createPromisedAction({
+                    type: getType(fetchIdeasAsync.success),
+                    callback: resolve,
+                  }),
+                );
+              });
+            }}
+            isRowLoaded={({ index }) => {
+              return !!ideas[index];
+            }}
+            rowCount={rowCount}
+          >
+            {({ onRowsRendered, registerChild }) => (
+              <AutoSizer>
+                {({ width, height }) => (
+                  <List
+                    rowCount={rowCount}
+                    ref={registerChild}
+                    onRowsRendered={onRowsRendered}
+                    width={width}
+                    height={height}
+                    rowHeight={ideaListItemFullHeight}
+                    noRowsRenderer={() =>
+                      showMyIdeas ? (
+                        <Box m={3}>
+                          <Typography variant="h6">
+                            I have no idea{' '}
+                            <span role="img" aria-label="thinking">
+                              🤔
+                            </span>{' '}
+                            Do you?
+                          </Typography>
+                          <Box mt={2}>
+                            <CreateIdea user={user} />
                           </Box>
-                        ) : (
-                          <IdeaRow key={idea.id} idea={idea} user={user} />
-                        )}
-                      </Box>
-                    );
-                  }}
-                />
-              )}
-            </AutoSizer>
-          )}
-        </InfiniteLoader>
-      </div>
-    </Box>
-  );
-};
+                        </Box>
+                      ) : (
+                        <Box>
+                          {range(0, count).map(() => (
+                            <IdeaSkeleton />
+                          ))}
+                        </Box>
+                      )
+                    }
+                    rowRenderer={({ key, index, style }) => {
+                      const idea = ideas[index];
 
-export const Ideas = connect((state: State) => {
-  // * useSelector was returning "[]" instead of "['loading', 'loading', 'loading'...]"
-  // * hence the loading state was not being rendered between filter updates
-  return { ideas: selectIdeas(state), count: selectCount(state) };
-})(IdeasComponent);
+                      return (
+                        <Box key={key} style={style}>
+                          {!idea ? null : idea === 'loading' ? (
+                            <IdeaOptionsSkeleton />
+                          ) : idea instanceof Error ? (
+                            <Box
+                              display="flex"
+                              alignItems="center"
+                              height="100%"
+                              mx={2}
+                            >
+                              <Typography
+                                style={{
+                                  textDecoration: 'underline',
+                                  cursor: 'pointer',
+                                }}
+                                onClick={() => {
+                                  const fetchOptions = {
+                                    startIndex: idea.startIndex,
+                                    stopIndex: idea.stopIndex,
+                                    fieldPath: idea.fieldPath,
+                                    opStr: idea.opStr,
+                                    value: idea.value,
+                                  };
+                                  fetchIdeas(fetchOptions);
+                                }}
+                              >
+                                Refetch ideas
+                              </Typography>
+                              &nbsp; from {idea.startIndex + 1} to{' '}
+                              {idea.stopIndex + 1}
+                            </Box>
+                          ) : (
+                            <IdeaRow key={idea.id} idea={idea} user={user} />
+                          )}
+                        </Box>
+                      );
+                    }}
+                  />
+                )}
+              </AutoSizer>
+            )}
+          </InfiniteLoader>
+        </div>
+      </Box>
+    );
+  },
+);
+
+export const Ideas = connect(
+  (state: State) => {
+    // * useSelector was returning "[]" instead of "['loading', 'loading', 'loading'...]"
+    // * hence the loading state was not being rendered between filter updates
+    return { ideas: selectIdeas(state), count: selectCount(state) };
+  },
+  null,
+  null,
+  { forwardRef: true },
+)(IdeasComponent);
--- src/pages/Idea/IdeasPage.tsx
+++ src/pages/Idea/IdeasPage.tsx
@@ -1,14 +1,72 @@
-import { Box } from '@material-ui/core';
+import { Box, Tab, Tabs } from '@material-ui/core';
 import { Ideas } from 'containers';
+import qs from 'qs';
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, useHistory, useLocation } from 'react-router-dom';
+import {
+  useSignedInUser,
+  useActions,
+  createSetIdeas,
+  createSetCount,
+  initialIdeasState,
+} from 'services';
+import { InfiniteLoader } from 'react-virtualized';
 
 export interface IdeasPageProps extends RouteComponentProps {}
 
 export const IdeasPage: React.FC<IdeasPageProps> = () => {
+  const history = useHistory();
+
+  const user = useSignedInUser();
+
+  const query = qs.parse(useLocation().search, { ignoreQueryPrefix: true });
+
+  const showMyIdeas = (query.author && query.author) === user?.email;
+
+  const infiniteLoaderRef = React.useRef<InfiniteLoader | null>(null);
+
+  const { setIdeas, setCount } = useActions({
+    setIdeas: createSetIdeas,
+    setCount: createSetCount,
+  });
+
+  const reset = () => {
+    setIdeas({ ideas: [] });
+    setCount({ count: initialIdeasState.count });
+
+    infiniteLoaderRef.current?.resetLoadMoreRowsCache();
+  };
+
   return (
     <Box>
-      <Ideas />
+      <Tabs
+        value={Number(showMyIdeas)}
+        indicatorColor="primary"
+        textColor="primary"
+        variant="fullWidth"
+      >
+        <Tab
+          label="Discover"
+          onClick={() => {
+            reset();
+
+            history.push({
+              search: qs.stringify({}),
+            });
+          }}
+        />
+        <Tab
+          label="Your Ideas"
+          onClick={() => {
+            reset();
+
+            history.push({
+              search: qs.stringify({ author: user?.email }),
+            });
+          }}
+        />
+      </Tabs>
+      <Ideas ref={infiniteLoaderRef} showMyIdeas={showMyIdeas} user={user} />
     </Box>
   );
 };
