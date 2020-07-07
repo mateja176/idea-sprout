1b921b885c11cdd3db4d2663b608108d87ceb081
mateja <mateja176@gmail.com>
2020-07-07 17:03:35 +0200

    feat(containers/Idea/Ideas/MyIdeas): extracts MyEmptyIdeas

--- src/containers/Idea/Ideas/MyIdeas.tsx
+++ src/containers/Idea/Ideas/MyIdeas.tsx
@@ -4,6 +4,22 @@ import React from 'react';
 import { useFirestoreCollection, useIdeasRef } from 'services';
 import { CreateIdea } from '../CreateIdea';
 import { IdeaRow } from '../IdeaRow';
+import { IdeasSkeleton } from './IdeasSkeleton';
+
+export const MyEmptyIdeas = ({ user }: { user: User }) => (
+  <Box m={3}>
+    <Typography variant="h6">
+      I have no idea{' '}
+      <span role="img" aria-label="thinking">
+        🤔
+      </span>{' '}
+      Do you?
+    </Typography>
+    <Box mt={2}>
+      <CreateIdea user={user} />
+    </Box>
+  </Box>
+);
 
 export const MyIdeas: React.FC<{ user: User }> = ({ user }) => {
   const ideas = useFirestoreCollection<IdeaModel>(
@@ -15,18 +31,9 @@ export const MyIdeas: React.FC<{ user: User }> = ({ user }) => {
   return (
     <Box>
       {ideas.length <= 0 ? (
-        <Box m={3}>
-          <Typography variant="h6">
-            I have no idea{' '}
-            <span role="img" aria-label="thinking">
-              🤔
-            </span>{' '}
-            Do you?
-          </Typography>
-          <Box mt={2}>
-            <CreateIdea user={user} />
-          </Box>
-        </Box>
+        // * Displaying a Skeleton is preferable considering the case of an user with a number ideas
+        // * landing on a page where he is prompted to created his or her first idea
+        <IdeasSkeleton />
       ) : (
         ideas.map((idea) => <IdeaRow key={idea.id} idea={idea} user={user} />)
       )}
