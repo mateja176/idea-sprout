f4c67f89e8bdabcf4cc80f1577a98ed84fc85965
mateja <mateja176@gmail.com>
2020-07-07 02:04:21 +0200

    feat(containers/Idea/Ideas): removes WindowScroller

--- src/containers/Idea/Ideas/Ideas.tsx
+++ src/containers/Idea/Ideas/Ideas.tsx
@@ -3,12 +3,7 @@ import { IdeaRow, IdeasSkeleton } from 'containers';
 import { IdeaFilter, User, WithCount } from 'models';
 import React from 'react';
 import { connect, useDispatch } from 'react-redux';
-import {
-  AutoSizer,
-  InfiniteLoader,
-  List,
-  WindowScroller,
-} from 'react-virtualized';
+import { AutoSizer, InfiniteLoader, List } from 'react-virtualized';
 import { useFirestoreDocData } from 'reactfire';
 import {
   createPromisedAction,
@@ -70,72 +65,64 @@ export const IdeasComponent = ({ user, ideas }: IdeasProps) => {
       rowCount={rowCount}
     >
       {({ onRowsRendered, registerChild }) => (
-        <WindowScroller>
-          {({ height, isScrolling, scrollTop, onChildScroll }) => (
-            <AutoSizer disableHeight>
-              {({ width }) => (
-                <List
-                  isScrolling={isScrolling}
-                  scrollTop={scrollTop}
-                  onScroll={onChildScroll}
-                  rowCount={rowCount}
-                  ref={registerChild}
-                  onRowsRendered={onRowsRendered}
-                  width={width}
-                  autoHeight
-                  height={height}
-                  rowHeight={ideaListItemFullHeight}
-                  noRowsRenderer={() => (
-                    <Box>
-                      <IdeasSkeleton />
-                    </Box>
-                  )}
-                  rowRenderer={({ key, index, style }) => {
-                    const idea = ideas[index];
-
-                    return (
-                      <Box key={key} style={style}>
-                        {!idea ? null : idea === 'loading' ? (
-                          <IdeaOptionsSkeleton />
-                        ) : idea instanceof Error ? (
-                          <Box
-                            display="flex"
-                            alignItems="center"
-                            height="100%"
-                            mx={2}
-                          >
-                            <Typography
-                              style={{
-                                textDecoration: 'underline',
-                                cursor: 'pointer',
-                              }}
-                              onClick={() => {
-                                const fetchOptions = {
-                                  startIndex: idea.startIndex,
-                                  stopIndex: idea.stopIndex,
-                                  fieldPath: idea.fieldPath,
-                                  opStr: idea.opStr,
-                                  value: idea.value,
-                                };
-                                fetchIdeas(fetchOptions);
-                              }}
-                            >
-                              Refetch ideas
-                            </Typography>
-                            &nbsp; from {idea.startIndex + 1} to{' '}
-                            {idea.stopIndex + 1}
-                          </Box>
-                        ) : (
-                          <IdeaRow key={idea.id} idea={idea} user={user} />
-                        )}
-                      </Box>
-                    );
-                  }}
-                />
+        <AutoSizer>
+          {({ width, height }) => (
+            <List
+              rowCount={rowCount}
+              ref={registerChild}
+              onRowsRendered={onRowsRendered}
+              width={width}
+              height={height}
+              rowHeight={ideaListItemFullHeight}
+              noRowsRenderer={() => (
+                <Box>
+                  <IdeasSkeleton />
+                </Box>
               )}
-            </AutoSizer>
+              rowRenderer={({ key, index, style }) => {
+                const idea = ideas[index];
+
+                return (
+                  <Box key={key} style={style}>
+                    {!idea ? null : idea === 'loading' ? (
+                      <IdeaOptionsSkeleton />
+                    ) : idea instanceof Error ? (
+                      <Box
+                        display="flex"
+                        alignItems="center"
+                        height="100%"
+                        mx={2}
+                      >
+                        <Typography
+                          style={{
+                            textDecoration: 'underline',
+                            cursor: 'pointer',
+                          }}
+                          onClick={() => {
+                            const fetchOptions = {
+                              startIndex: idea.startIndex,
+                              stopIndex: idea.stopIndex,
+                              fieldPath: idea.fieldPath,
+                              opStr: idea.opStr,
+                              value: idea.value,
+                            };
+                            fetchIdeas(fetchOptions);
+                          }}
+                        >
+                          Refetch ideas
+                        </Typography>
+                        &nbsp; from {idea.startIndex + 1} to{' '}
+                        {idea.stopIndex + 1}
+                      </Box>
+                    ) : (
+                      <IdeaRow key={idea.id} idea={idea} user={user} />
+                    )}
+                  </Box>
+                );
+              }}
+            />
           )}
-        </WindowScroller>
+        </AutoSizer>
       )}
     </InfiniteLoader>
   );
