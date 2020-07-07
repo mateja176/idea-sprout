f46fde4ea5f555ac70f856ca3223b24c4e7147ec
mateja <mateja176@gmail.com>
2020-07-07 15:51:59 +0200

    fix(services/store/epic/ideas): select lastIdea
    right before request a new batch

--- src/services/store/epic/ideas.ts
+++ src/services/store/epic/ideas.ts
@@ -5,13 +5,7 @@ import { findLast } from 'ramda';
 import { Epic, ofType } from 'redux-observable';
 import { collection } from 'rxfire/firestore';
 import { of } from 'rxjs';
-import {
-  catchError,
-  concatMap,
-  first,
-  map,
-  withLatestFrom,
-} from 'rxjs/operators';
+import { catchError, concatMap, first, map, mergeMap } from 'rxjs/operators';
 import { Action, State } from 'services';
 import { getType } from 'typesafe-actions';
 import { convertFirestoreDocument, firestoreCollections } from 'utils';
@@ -35,50 +29,53 @@ export const fetch: Epic<
 > = (action$, state$) =>
   action$.pipe(
     ofType<Action, FetchIdeasRequest>(getType(fetchIdeasAsync.request)),
-    withLatestFrom(state$),
     // * startAfter requires the last batch to be loaded before requesting a new one
     concatMap(
-      ([
-        {
-          payload: {
-            startIndex,
-            stopIndex,
-            fieldPath,
-            opStr,
-            value,
-            directionStr = 'desc' as firebase.firestore.OrderByDirection,
-            orderByField = 'createdAt',
-          },
+      ({
+        payload: {
+          startIndex,
+          stopIndex,
+          fieldPath,
+          opStr,
+          value,
+          directionStr = 'desc' as firebase.firestore.OrderByDirection,
+          orderByField = 'createdAt',
         },
-        state,
-      ]) => {
-        const lastIdea = findLast(isIdea, selectIdeas(state));
-
+      }) => {
         const limit = stopIndex - startIndex;
 
-        return collection(
-          firebase
-            .firestore()
-            .collection(firestoreCollections.ideas.path)
-            .where(fieldPath, opStr, value)
-            .orderBy(orderByField, directionStr)
-            .startAfter(isIdea(lastIdea) ? lastIdea.createdAt : '')
-            .limit(limit),
-        ).pipe(
-          first(), // * necessary because of concatMap, without it the observable would never complete
-          map((snapshots) =>
-            fetchIdeasAsync.success({
-              startIndex,
-              stopIndex,
-              ideas: snapshots.map((snapshot) =>
-                convertFirestoreDocument<IdeaModel>(snapshot),
+        return state$
+          .pipe(
+            map(selectIdeas),
+            map(findLast(isIdea)),
+            first(), // * avoids multiple requests
+            mergeMap((lastIdea) =>
+              collection(
+                firebase
+                  .firestore()
+                  .collection(firestoreCollections.ideas.path)
+                  .where(fieldPath, opStr, value)
+                  .orderBy(orderByField, directionStr)
+                  .startAfter(isIdea(lastIdea) ? lastIdea.createdAt : '')
+                  .limit(limit),
               ),
-            }),
-          ),
-          catchError((error: FirebaseError) =>
-            of(fetchIdeasAsync.failure(error)),
-          ),
-        );
+            ),
+          )
+          .pipe(
+            first(), // * necessary because of concatMap, without it the observable would never complete
+            map((snapshots) =>
+              fetchIdeasAsync.success({
+                startIndex,
+                stopIndex,
+                ideas: snapshots.map((snapshot) =>
+                  convertFirestoreDocument<IdeaModel>(snapshot),
+                ),
+              }),
+            ),
+            catchError((error: FirebaseError) =>
+              of(fetchIdeasAsync.failure(error)),
+            ),
+          );
       },
     ),
   );
