847de4772f8ca5f281951bd0b53db6d9a7658341
mateja <mateja176@gmail.com>
2020-07-11 23:42:06 +0200

    perf(containers/DraggableDialog): wraps in memo

--- src/containers/DraggableDialog.tsx
+++ src/containers/DraggableDialog.tsx
@@ -19,30 +19,27 @@ export interface DraggableDialogProps extends DialogProps, WithMinWidth {
 
 const dragHandleId = 'drag-handle';
 
-export const DraggableDialog: React.FC<DraggableDialogProps> = ({
-  dialogTitle,
-  children,
-  actions,
-  ...props
-}) => {
-  const { scroll } = props;
+export const DraggableDialog = React.memo<DraggableDialogProps>(
+  ({ dialogTitle, children, actions, ...props }) => {
+    const { scroll } = props;
 
-  return (
-    <Dialog // TODO replace modal with custom implementation since elements outside of the dialog cannot be interacted with
-      {...props}
-      hideBackdrop
-      PaperComponent={DraggablePaper}
-    >
-      <DialogTitle style={{ cursor: 'grab' }}>
-        <Box display="flex" alignItems="center">
-          {dialogTitle}
-          <Box ml="auto" id={dragHandleId}>
-            <DragIndicator color="action" />
+    return (
+      <Dialog // TODO replace modal with custom implementation since elements outside of the dialog cannot be interacted with
+        {...props}
+        hideBackdrop
+        PaperComponent={DraggablePaper}
+      >
+        <DialogTitle style={{ cursor: 'grab' }}>
+          <Box display="flex" alignItems="center">
+            {dialogTitle}
+            <Box ml="auto" id={dragHandleId}>
+              <DragIndicator color="action" />
+            </Box>
           </Box>
-        </Box>
-      </DialogTitle>
-      <DialogContent dividers={scroll === 'paper'}>{children}</DialogContent>
-      {actions && <DialogActions>{actions}</DialogActions>}
-    </Dialog>
-  );
-};
+        </DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>{children}</DialogContent>
+        {actions && <DialogActions>{actions}</DialogActions>}
+      </Dialog>
+    );
+  },
+);
