c164c571649083fef416104ef1ecbe3d9841c0ca
mateja <mateja176@gmail.com>
2020-07-14 23:40:47 +0200

    feat(containers/Idea/Ideas): adds AutoSizer to avoid scroll jumps

--- src/containers/Idea/Ideas/IdeasSkeleton.tsx
+++ src/containers/Idea/Ideas/IdeasSkeleton.tsx
@@ -1,6 +1,7 @@
-import { List } from '@material-ui/core';
+import { Box, List } from '@material-ui/core';
 import { range } from 'ramda';
 import React from 'react';
+import { AutoSizer } from 'react-virtualized';
 import { ideaListStyle } from 'styles';
 import { ideasFetchLimit } from 'utils';
 import { IdeaOptionsSkeleton } from '../IdeaOptionsSkeleton';
@@ -9,10 +10,16 @@ export interface IdeasSkeletonProps {}
 
 export const IdeasSkeleton: React.FC<IdeasSkeletonProps> = () => {
   return (
-    <List style={ideaListStyle}>
-      {range(0, ideasFetchLimit).map((i) => (
-        <IdeaOptionsSkeleton key={i} />
-      ))}
-    </List>
+    <AutoSizer>
+      {(size) => (
+        <Box {...size} overflow={'auto'}>
+          <List style={ideaListStyle}>
+            {range(0, ideasFetchLimit).map((i) => (
+              <IdeaOptionsSkeleton key={i} />
+            ))}
+          </List>
+        </Box>
+      )}
+    </AutoSizer>
   );
 };
