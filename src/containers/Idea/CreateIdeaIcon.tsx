159afea3498ec81754cf1ec23b587ddc1744f762
mateja <mateja176@gmail.com>
2020-07-17 02:39:23 +0200

    feat(containers/Idea): hides create icon if signed out

--- src/containers/Idea/CreateIdeaIcon.tsx
+++ src/containers/Idea/CreateIdeaIcon.tsx
@@ -1,16 +1,20 @@
 import { IconButton, Tooltip } from '@material-ui/core';
 import { LibraryAdd } from '@material-ui/icons';
+import { User } from 'models';
 import React from 'react';
+import { useUser } from 'reactfire';
 import { useCreateIdea } from 'services';
 
 export const CreateIdeaIcon = () => {
+  const user = useUser<User | null>();
+
   const { create, loading } = useCreateIdea();
 
-  return (
+  return user ? (
     <Tooltip title={'Create Idea'}>
       <IconButton color={'inherit'} disabled={loading} onClick={create}>
         <LibraryAdd />
       </IconButton>
     </Tooltip>
-  );
+  ) : null;
 };
