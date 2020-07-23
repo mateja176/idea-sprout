b3c322fe10a79f32f8c84843c676510f6160e7bd
mateja <mateja176@gmail.com>
2020-07-23 18:06:21 +0200

    fix(containers/Idea/Review/Export): does not perform side effects in useEffect

--- src/containers/Idea/Review/ExportReviews.tsx
+++ src/containers/Idea/Review/ExportReviews.tsx
@@ -1,14 +1,26 @@
 import {
   Box,
   Button,
+  CircularProgress,
   Dialog,
   DialogActions,
+  DialogContent,
   DialogTitle,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+  makeStyles,
   Tab,
   TabProps,
   Tooltip,
 } from '@material-ui/core';
-import { CloudDownload, CloudUpload, EmojiEvents } from '@material-ui/icons';
+import {
+  CloudDownload,
+  CloudUpload,
+  EmojiEvents,
+  ExpandMore,
+} from '@material-ui/icons';
+import { Skeleton } from '@material-ui/lab';
 import { useBoolean } from 'ahooks';
 import { proMembership, proMembershipDiscount } from 'elements';
 import { parseAsync } from 'json2csv';
@@ -17,29 +29,59 @@ import {
   claims,
   FirestoreUser,
   IdeaModel,
+  Order,
   Review,
   ReviewWithAuthor,
   User,
+  WithPaypal,
 } from 'models';
 import React from 'react';
 import { AuthCheck } from 'reactfire';
 import {
+  createQueueSnackbar,
+  Email,
+  env,
   formatCurrency,
-  useBooleanWithFallback,
+  useActions,
   useReviewsRef,
   useUsersRef,
 } from 'services';
-import { tabChildStyle } from 'styles';
+import {
+  paypalButtonsHeight,
+  paypalHeightBreakpoint,
+  tabChildStyle,
+} from 'styles';
 import { convertFirestoreCollection, convertFirestoreDocument } from 'utils';
-import { ExportReviewsContent } from './ExportReviewsContent';
+
+const id = 'paypal-container';
+
+const scriptId = 'paypal-script';
+
+const useStyles = makeStyles((theme) => ({
+  paypalButtonsHeight: {
+    display: 'flex',
+    justifyContent: 'center',
+    height: paypalButtonsHeight[116],
+    [theme.breakpoints.up(paypalHeightBreakpoint[400])]: {
+      height: paypalButtonsHeight[142],
+    },
+    [theme.breakpoints.up(paypalHeightBreakpoint[600])]: {
+      height: paypalButtonsHeight[168],
+    },
+  },
+}));
 
 export const ExportReviews: React.FC<
   { idea: IdeaModel; isAuthor: boolean } & Required<Pick<User, 'email'>> &
     Pick<TabProps, 'classes'>
 > = ({ idea, isAuthor, email, ...props }) => {
+  const { queueSnackbar } = useActions({ queueSnackbar: createQueueSnackbar });
+
   const reviewsRef = useReviewsRef(idea.id);
 
   const usersRef = useUsersRef();
+
+  const classes = useStyles();
 
   const [loading, setLoading] = useBoolean();
 
@@ -92,17 +134,92 @@ export const ExportReviews: React.FC<
 
   const [upgradeDialogOpen, setUpgradeDialogOpen] = useBoolean();
 
-  const openWithFallback = useBooleanWithFallback(upgradeDialogOpen, {
-    timeoutMs: 500,
-  });
+  const [scriptLoading, setScriptLoading] = useBoolean();
+
+  const [sendingEmail, setSendingEmail] = useBoolean();
+
+  const sendEmail = React.useCallback(
+    (order: Order) => {
+      setSendingEmail.setTrue();
+      return Email.send({
+        Host: env.smtpHost,
+        Username: env.smtpUsername,
+        Password: env.smtpPassword,
+        To: env.smtpTo,
+        From: env.smtpFrom,
+        Subject: 'Pro Membership Order',
+        Body: `- Order placed by: ${email}
+- Order id is: ${order.id}`,
+      }).then(() => {
+        setSendingEmail.setFalse();
+
+        setUpgradeDialogOpen.setFalse();
+
+        queueSnackbar({
+          severity: 'success',
+          message:
+            "You'll receive an email, up to 12 hours from now, stating that your membership is active",
+          autoHideDuration: 10000,
+        });
+      });
+    },
+    [email, queueSnackbar, setUpgradeDialogOpen, setSendingEmail],
+  );
+
+  const renderButtons = React.useCallback(() => {
+    ((window as unknown) as WithPaypal).paypal
+      ?.Buttons({
+        createOrder: (_, actions) =>
+          actions.order.create({
+            purchase_units: [
+              {
+                description: 'Pro membership',
+                amount: {
+                  value: proMembershipDiscount.amount.value,
+                },
+              },
+            ],
+          }),
+        onApprove: (_, actions) => actions.order.capture().then(sendEmail),
+      })
+      .render(`#${id}`);
+  }, [sendEmail]);
+
+  const loadScript = React.useCallback(() => {
+    setScriptLoading.setTrue();
+
+    const script = document.createElement('script');
+    script.src = `https://www.paypal.com/sdk/js?client-id=${env.paypalClientId}`;
+    script.id = scriptId;
+
+    script.addEventListener('load', () => {
+      setScriptLoading.setFalse();
+
+      renderButtons();
+    });
+
+    document.body.appendChild(script);
+  }, [renderButtons, setScriptLoading]);
+
+  const openUpgradeDialog = React.useCallback(() => {
+    setUpgradeDialogOpen.setTrue();
+  }, [setUpgradeDialogOpen]);
 
   const becomeAPro = (
     <Tooltip title={'Become a Pro'}>
-      <Box style={tabChildStyle} onClick={setUpgradeDialogOpen.setTrue}>
+      <Box style={tabChildStyle} onClick={openUpgradeDialog}>
         <EmojiEvents color={'secondary'} />
       </Box>
     </Tooltip>
   );
+
+  const handleEntered = React.useCallback(() => {
+    if (document.getElementById(scriptId)) {
+      renderButtons();
+    } else {
+      loadScript();
+    }
+  }, [renderButtons, loadScript]);
 
   return (
     <>
@@ -115,10 +232,7 @@ export const ExportReviews: React.FC<
             fallback={
               isAuthor ? (
                 <Tooltip title={'Export reviews'}>
-                  <Box
-                    style={tabChildStyle}
-                    onClick={setUpgradeDialogOpen.setTrue}
-                  >
+                  <Box style={tabChildStyle} onClick={openUpgradeDialog}>
                     <CloudUpload color={'secondary'} />
                   </Box>
                 </Tooltip>
@@ -139,7 +253,7 @@ export const ExportReviews: React.FC<
           </AuthCheck>
         }
       />
-      <Dialog open={upgradeDialogOpen} fullScreen>
+      <Dialog open={upgradeDialogOpen} fullScreen onEntered={handleEntered}>
         <DialogTitle>
           Become a Pro:{' '}
           <span style={{ textDecoration: 'line-through' }}>
@@ -147,12 +261,44 @@ export const ExportReviews: React.FC<
           </span>{' '}
           <span>{formatCurrency(proMembershipDiscount.amount.value)}</span>
         </DialogTitle>
-        {openWithFallback && (
-          <ExportReviewsContent
-            email={email}
-            onClose={setUpgradeDialogOpen.setFalse}
-          />
-        )}
+        <DialogContent style={{ overflowY: 'scroll' }}>
+          <Box textAlign={'justify'} position={'relative'}>
+            {sendingEmail && (
+              <Box
+                position={'absolute'}
+                display={'flex'}
+                width={'100%'}
+                justifyContent={'center'}
+                height={'100%'}
+                alignItems={'center'}
+              >
+                <CircularProgress />
+              </Box>
+            )}
+            <Box visibility={sendingEmail ? 'hidden' : 'visible'}>
+              <Box>
+                {proMembership.proposition}
+                <br />
+                {proMembershipDiscount.proposition}
+                <br />
+                <ExpansionPanel>
+                  <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+                    Important note
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                    {proMembership.info}
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              </Box>
+              <br />
+              <div id={id} className={classes.paypalButtonsHeight}>
+                {scriptLoading && (
+                  <Skeleton width={'100%'} height={'100%'} variant={'rect'} />
+                )}
+              </div>
+            </Box>
+          </Box>
+        </DialogContent>
         <DialogActions>
           <Button onClick={setUpgradeDialogOpen.setFalse}>Close</Button>
         </DialogActions>
