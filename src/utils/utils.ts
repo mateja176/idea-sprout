caa88ee3d6988582cd4a5bfc13df990e7baf3c1b
mateja <mateja176@gmail.com>
2020-07-29 22:01:46 +0200

    adds readonly constraint

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,2 @@
-export const arrayToMap = <V extends string | number>(a: V[]) =>
+export const arrayToMap = <V extends string | number>(a: ReadonlyArray<V>) =>
   Object.fromEntries(a.map((v) => [v, v])) as Record<V, V>;
