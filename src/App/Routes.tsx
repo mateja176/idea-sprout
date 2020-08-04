b7da845509f2d6ce4438c2bc72367cfb6bcda32a
mateja <mateja176@gmail.com>
2020-08-04 15:26:42 +0200

    fix(App/Routes): register was not working
    because user and set User weren't passed down

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -17,58 +17,59 @@ const NotFound: React.FC<RouteComponentProps> = () => (
   </Box>
 );
 
-const SigninComponent: React.FC<
-  RouteComponentProps & WithUserState & Pick<RoutesProps, 'setUserState'>
-> = ({ user, setUserState, ...props }) => {
-  const {
-    location: { pathname },
-  } = props;
-
-  if (isUserLoading(user)) {
-    if (pathname === absolutePrivateRoute.ideas.path || pathname === '/') {
-      return (
-        <Box
-          flex={1}
-          display={'flex'}
-          flexDirection={'column'}
-          overflow={'auto'}
-        >
-          <Tabs value={false} variant={'fullWidth'}>
-            <Load boxFlex={1}>
-              <Tab />
-            </Load>
-            <Load boxFlex={1}>
-              <Tab />
-            </Load>
-          </Tabs>
-          <IdeasSkeleton />
-        </Box>
-      );
-    } else if (
-      /^\/[\w\d]+$/.test(
-        pathname.split(absolutePrivateRoute.ideas.path).join(''),
-      )
-    ) {
-      return <IdeaContainerSkeleton />;
-    } else {
-      return <NotFound {...props} />;
-    }
-  } else if (user === null) {
-    return <Signin setUserState={setUserState} user={user} {...props} />;
-  } else {
-    return <Signin setUserState={setUserState} user={user} {...props} />;
-  }
-};
-
 const RedirectToIdeas: React.FC<RouteComponentProps> = () => (
   <Redirect to={absolutePrivateRoute.ideas.path} />
 );
 
 export const Routes: React.FC<RoutesProps> = ({ user, setUserState }) => {
+  const RenderSignin = React.useCallback(
+    (props: RouteComponentProps) => {
+      const {
+        location: { pathname },
+      } = props;
+
+      if (isUserLoading(user)) {
+        if (pathname === absolutePrivateRoute.ideas.path || pathname === '/') {
+          return (
+            <Box
+              flex={1}
+              display={'flex'}
+              flexDirection={'column'}
+              overflow={'auto'}
+            >
+              <Tabs value={false} variant={'fullWidth'}>
+                <Load boxFlex={1}>
+                  <Tab />
+                </Load>
+                <Load boxFlex={1}>
+                  <Tab />
+                </Load>
+              </Tabs>
+              <IdeasSkeleton />
+            </Box>
+          );
+        } else if (
+          /^\/[\w\d]+$/.test(
+            pathname.split(absolutePrivateRoute.ideas.path).join(''),
+          )
+        ) {
+          return <IdeaContainerSkeleton />;
+        } else {
+          return <NotFound {...props} />;
+        }
+      } else if (user === null) {
+        return <Signin setUserState={setUserState} user={user} {...props} />;
+      } else {
+        return <Signin setUserState={setUserState} user={user} {...props} />;
+      }
+    },
+    [user, setUserState],
+  );
+
   return (
     <Switch>
       {isUserLoading(user) || user === null || !user.emailVerified ? (
-        <Route component={SigninComponent} />
+        <Route render={RenderSignin} />
       ) : null}
       <Route
         exact
