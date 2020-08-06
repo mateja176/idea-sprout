7c0de5526ac7a71be34403c72d94c1e89f5c2fe3
mateja <mateja176@gmail.com>
2020-08-06 14:56:39 +0200

    hides menu button instead of unmounting it

--- src/containers/MenuButton.tsx
+++ src/containers/MenuButton.tsx
@@ -1,3 +1,4 @@
+import Box from '@material-ui/core/Box';
 import { ButtonProps } from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/icons/Menu';
@@ -14,14 +15,16 @@ export const MenuButton: React.FC<Pick<ButtonProps, 'onClick'>> = ({
   const user = useUserState();
   const isSignedIn = getIsSignedIn(user);
 
-  return isSignedIn ? (
-    <IconButton
-      onClick={onClick}
-      edge="start"
-      color="inherit"
-      aria-label={'Menu'}
-    >
-      <Menu />
-    </IconButton>
-  ) : null;
+  return (
+    <Box visibility={isSignedIn ? 'visible' : 'hidden'}>
+      <IconButton
+        onClick={onClick}
+        edge="start"
+        color="inherit"
+        aria-label={'Menu'}
+      >
+        <Menu />
+      </IconButton>
+    </Box>
+  );
 };
