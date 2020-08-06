72347882e904e07dc8390f17bcd6df9bdcdb3a13
mateja <mateja176@gmail.com>
2020-08-06 11:28:40 +0200

    fix(services/hooks/idea#useIdeaUrl): adds ideas to shared path

--- src/services/hooks/idea.ts
+++ src/services/hooks/idea.ts
@@ -1,6 +1,7 @@
 import { IdeaModel } from 'models';
 import { useEffect, useMemo, useState } from 'react';
 import urljoin from 'url-join';
+import { absolutePrivateRoute } from 'utils';
 
 export const useIdeaUrl = (id: IdeaModel['id']) => {
   const [origin, setOrigin] = useState('');
@@ -8,5 +9,8 @@ export const useIdeaUrl = (id: IdeaModel['id']) => {
     setOrigin(window.location.origin);
   }, []);
 
-  return useMemo(() => urljoin(origin, id), [origin, id]);
+  return useMemo(() => urljoin(origin, absolutePrivateRoute.ideas.path, id), [
+    origin,
+    id,
+  ]);
 };
