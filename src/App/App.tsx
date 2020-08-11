9ca7cc8f222bffd718b851ceb9932861b9d5943c
mateja <mateja176@gmail.com>
2020-08-11 23:42:37 +0200

    refactor: adds Provider folder

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import { Layout } from './Layout/Layout';
-import { Provider } from './Provider';
+import { Provider } from './Provider/Provider';
 import { Routes } from './Routes/Routes';
 import { RoutesSkeleton } from './Routes/RoutesSkeleton';
 import { Snackbar } from './Snackbar';
