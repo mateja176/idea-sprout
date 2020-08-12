4a6aeb7a51074d1291433286ddf04d573a252ad0
mateja <mateja176@gmail.com>
2020-08-12 14:01:17 +0200

    refactor(utils/styles): prefers relative import path

--- src/utils/styles/paypal.ts
+++ src/utils/styles/paypal.ts
@@ -1,4 +1,4 @@
-import { arrayToMap } from 'utils/utils';
+import { arrayToMap } from '../utils';
 
 export const paypalButtonsHeight = arrayToMap([116, 142, 168]);
 export const paypalHeightBreakpoint = arrayToMap([0, 400, 600]);
