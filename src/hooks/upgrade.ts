5418dd54b827922a8b136d633d5d5eb43d019baa
mateja <mateja176@gmail.com>
2020-08-15 23:37:06 +0200

    fix(hooks/upgrade): the unused expression was not being caught by eslint

--- src/hooks/upgrade.ts
+++ src/hooks/upgrade.ts
@@ -2,5 +2,5 @@ import React from 'react';
 import { renderButtons, RenderButtonsParams } from 'services/upgrade';
 
 export const useRenderButtons = (params: RenderButtonsParams) => {
-  return React.useCallback(() => renderButtons(params), [params]);
+  return React.useCallback(renderButtons(params), [params]);
 };
