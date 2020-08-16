99836d8fd172ae3cc86f824f0c58112ce5da2693
mateja <mateja176@gmail.com>
2020-08-16 14:56:16 +0200

    refactor(App/Routes/Skeleton): replaces if checks with Switch

--- src/App/Routes/RoutesSkeleton.tsx
+++ src/App/Routes/RoutesSkeleton.tsx
@@ -1,21 +1,21 @@
 import { NotFound } from 'components/NotFound';
 import { IdeaContainerSkeleton } from 'containers/Idea/IdeaContainerSkeleton';
-import { absolutePrivateRoute } from 'elements/routes';
+import { absolutePrivateRoute, ideaPath } from 'elements/routes';
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { IdeasPageSkeleton } from '../pages/Idea/IdeasPageSkeleton';
 
 export const RoutesSkeleton: React.FC = () => {
-  const location = useLocation();
-  const { pathname } = location;
-
-  if (pathname === absolutePrivateRoute.ideas.path || pathname === '/') {
-    return <IdeasPageSkeleton />;
-  } else if (
-    /^\/[\w\d]+$/.test(pathname.split(absolutePrivateRoute.ideas.path).join(''))
-  ) {
-    return <IdeaContainerSkeleton />;
-  } else {
-    return <NotFound {...location} />;
-  }
+  return (
+    <Switch>
+      <Route
+        exact
+        path={absolutePrivateRoute.ideas.path}
+        component={IdeasPageSkeleton}
+      />
+      <Route exact path={'/'} component={IdeasPageSkeleton} />
+      <Route exact path={ideaPath} component={IdeaContainerSkeleton} />
+      <Route component={NotFound} />
+    </Switch>
+  );
 };
