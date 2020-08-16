2f1fcde0445aecec06dd422a8cdbfbadeb055713
mateja <mateja176@gmail.com>
2020-08-16 13:16:08 +0200

    fix(utils): makes arrayToMap ReturnType more specific

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,2 +1,2 @@
 export const arrayToMap = <V extends string | number>(a: ReadonlyArray<V>) =>
-  Object.fromEntries(a.map((v) => [v, v])) as Record<V, V>;
+  (Object.fromEntries(a.map((v) => [v, v])) as unknown) as { [key in V]: key };
