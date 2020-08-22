ebcae786528708b78c96dc00c363a792abf33c94
mateja <mateja176@gmail.com>
2020-08-22 20:13:49 +0200

    feat(index.tsx): if there is preloaded data, hydrates the page

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { preloadReady } from 'react-loadable';
 import { App } from './App/App';
 import './index.css';
+import { WithPreloadedIdea } from './models/idea';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  globalThis.document.getElementById('root'),
-);
+if ((globalThis as typeof globalThis & WithPreloadedIdea).__PRELOADED_IDEA__) {
+  globalThis.addEventListener('load', () => {
+    preloadReady().then(() => {
+      ReactDOM.hydrate(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+        document.getElementById('root'),
+      );
+    });
+  });
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    globalThis.document.getElementById('root'),
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
