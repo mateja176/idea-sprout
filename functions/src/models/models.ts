63b0faf5aa27639b36b3faeb587c6e1915fef810
mateja <mateja176@gmail.com>
2020-08-23 20:18:40 +0200

    feat(functions/models): extends Config with env vars for App

--- functions/src/models/models.ts
+++ functions/src/models/models.ts
@@ -6,4 +6,20 @@ export interface Config {
     oauth_api: string;
     amount: string;
   };
+  config: {
+    api_key: string;
+    auth_domain: string;
+    database_url: string;
+    project_id: string;
+    storage_bucket: string;
+    messaging_sender_id: string;
+    app_id: string;
+    measurement_id: string;
+  };
+  log_rocket: {
+    id: string;
+  };
+  drift: {
+    id: string;
+  };
 }
