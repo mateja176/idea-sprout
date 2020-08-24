f78e98feb1798e0c2603392733635c77a310e2a9
mateja <mateja176@gmail.com>
2020-08-24 19:30:53 +0200

    deos not remove styles form the server

--- src/App/Provider/ThemeProvider.tsx
+++ src/App/Provider/ThemeProvider.tsx
@@ -1,12 +1,7 @@
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
-import { removeJSSStyle } from '../../services/services';
 import theme from '../../utils/styles/theme';
 
 export const ThemeProvider: React.FC = ({ children }) => {
-  React.useEffect(() => {
-    removeJSSStyle();
-  }, []);
-
   return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
 };
