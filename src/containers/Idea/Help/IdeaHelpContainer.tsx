1c7f264cc211eff2e9580194ee2737c31f3bf441
mateja <mateja176@gmail.com>
2020-08-24 18:35:50 +0200

    adds suspense

--- src/containers/Idea/Help/IdeaHelpContainer.tsx
+++ src/containers/Idea/Help/IdeaHelpContainer.tsx
@@ -1,5 +1,7 @@
+import Help from '@material-ui/icons/Help';
 import React from 'react';
 import { useRouteMatch } from 'react-router-dom';
+import { Load } from '../../../components/Load';
 import { ideaPath } from '../../../elements/routes';
 import { useMaybeUser } from '../../../hooks/firebase';
 import { WithId } from '../../../models/models';
@@ -14,5 +16,15 @@ export const IdeaHelpContainer = () => {
 
   const user = useMaybeUser();
 
-  return match && user ? <IdeaHelp id={match.params.id} user={user} /> : null;
+  return match && user ? (
+    <React.Suspense
+      fallback={
+        <Load>
+          <Help />
+        </Load>
+      }
+    >
+      <IdeaHelp id={match.params.id} user={user} />
+    </React.Suspense>
+  ) : null;
 };
