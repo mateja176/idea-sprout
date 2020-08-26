a25e2b88da8feb49ef06985d9c950923ccb48035
mateja <mateja176@gmail.com>
2020-08-26 15:41:07 +0200

    feat(functions): removes renderIdea
    since it was causing
    Function failed on loading user code. Error message: Error: please examine your function logs to see the error cause: https://cloud.google.com/functions/docs/monitoring/logging#viewing_logs

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 import * as admin from 'firebase-admin';
 import { adjustIdeaCount } from './adjustIdeaCount';
 import { adjustIdeaRating } from './adjustIdeaRating';
-import { renderIdea } from './renderIdea';
 import { upgradeToPro } from './upgradeToPro';
 
 admin.initializeApp();
 
-export { adjustIdeaCount, adjustIdeaRating, upgradeToPro, renderIdea };
+export { adjustIdeaCount, adjustIdeaRating, upgradeToPro };
