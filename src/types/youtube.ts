a3822d87a536a515c9c6eec647b71c005acf1765
mateja <mateja176@gmail.com>
2020-09-13 12:18:50 +0200

    imports CSS Properties from react

--- src/types/youtube.ts
+++ src/types/youtube.ts
@@ -1,4 +1,4 @@
-import { CSSProperties } from '@material-ui/core/styles/withStyles';
+import { CSSProperties } from 'react';
 
 interface Options extends Pick<CSSProperties, 'width' | 'height'> {
   videoId: string;
